8d31e7a7b584b8337fdb7c361ca06683e4647b81
tatianasnook <tatsnook@gmail.com>
2024-08-20 12:55:20 -0500

    added mobile menu

--- src/components/aboutus/AboutUs.jsx
+++ src/components/aboutus/AboutUs.jsx
@@ -5,6 +5,71 @@ const AboutUs = () => {
     <div className='about-container'>
       <h1>About us</h1>
       <h2>Handyman service near you</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore modi aliquam molestias 
+        atque provident soluta ducimus perferendis vero dolor cupiditate adipisci, tenetur 
+        officia eos commodi, odio nesciunt, ipsum magnam ipsam. Quaerat sint facere quae, 
+        unde enim distinctio deleniti repellendus accusamus quam incidunt cupiditate qui 
+        similique laborum nesciunt? Saepe incidunt officia dolor vitae nisi? Exercitationem 
+        eligendi ratione doloribus fuga, nisi provident minus officia dolorem placeat 
+        dignissimos ipsum consectetur tempora, rem deserunt recusandae accusamus at 
+        alias mollitia aperiam ullam maxime. Eligendi provident quo beatae eveniet inventore
+        dolores velit assumenda sequi est, tenetur ullam in dignissimos, alias temporibus 
+        consequuntur. Dolor consequuntur veniam atque.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore modi aliquam molestias 
+        atque provident soluta ducimus perferendis vero dolor cupiditate adipisci, tenetur 
+        officia eos commodi, odio nesciunt, ipsum magnam ipsam. Quaerat sint facere quae, 
+        unde enim distinctio deleniti repellendus accusamus quam incidunt cupiditate qui 
+        similique laborum nesciunt? Saepe incidunt officia dolor vitae nisi? Exercitationem 
+        eligendi ratione doloribus fuga, nisi provident minus officia dolorem placeat 
+        dignissimos ipsum consectetur tempora, rem deserunt recusandae accusamus at 
+        alias mollitia aperiam ullam maxime. Eligendi provident quo beatae eveniet inventore
+        dolores velit assumenda sequi est, tenetur ullam in dignissimos, alias temporibus 
+        consequuntur. Dolor consequuntur veniam atque.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore modi aliquam molestias 
+        atque provident soluta ducimus perferendis vero dolor cupiditate adipisci, tenetur 
+        officia eos commodi, odio nesciunt, ipsum magnam ipsam. Quaerat sint facere quae, 
+        unde enim distinctio deleniti repellendus accusamus quam incidunt cupiditate qui 
+        similique laborum nesciunt? Saepe incidunt officia dolor vitae nisi? Exercitationem 
+        eligendi ratione doloribus fuga, nisi provident minus officia dolorem placeat 
+        dignissimos ipsum consectetur tempora, rem deserunt recusandae accusamus at 
+        alias mollitia aperiam ullam maxime. Eligendi provident quo beatae eveniet inventore
+        dolores velit assumenda sequi est, tenetur ullam in dignissimos, alias temporibus 
+        consequuntur. Dolor consequuntur veniam atque.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore modi aliquam molestias 
+        atque provident soluta ducimus perferendis vero dolor cupiditate adipisci, tenetur 
+        officia eos commodi, odio nesciunt, ipsum magnam ipsam. Quaerat sint facere quae, 
+        unde enim distinctio deleniti repellendus accusamus quam incidunt cupiditate qui 
+        similique laborum nesciunt? Saepe incidunt officia dolor vitae nisi? Exercitationem 
+        eligendi ratione doloribus fuga, nisi provident minus officia dolorem placeat 
+        dignissimos ipsum consectetur tempora, rem deserunt recusandae accusamus at 
+        alias mollitia aperiam ullam maxime. Eligendi provident quo beatae eveniet inventore
+        dolores velit assumenda sequi est, tenetur ullam in dignissimos, alias temporibus 
+        consequuntur. Dolor consequuntur veniam atque.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore modi aliquam molestias 
+        atque provident soluta ducimus perferendis vero dolor cupiditate adipisci, tenetur 
+        officia eos commodi, odio nesciunt, ipsum magnam ipsam. Quaerat sint facere quae, 
+        unde enim distinctio deleniti repellendus accusamus quam incidunt cupiditate qui 
+        similique laborum nesciunt? Saepe incidunt officia dolor vitae nisi? Exercitationem 
+        eligendi ratione doloribus fuga, nisi provident minus officia dolorem placeat 
+        dignissimos ipsum consectetur tempora, rem deserunt recusandae accusamus at 
+        alias mollitia aperiam ullam maxime. Eligendi provident quo beatae eveniet inventore
+        dolores velit assumenda sequi est, tenetur ullam in dignissimos, alias temporibus 
+        consequuntur. Dolor consequuntur veniam atque.
+      </p>
+      <br />
     </div>
   )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,34 @@
 import { Link } from 'react-router-dom';
 import logo from "../../icons/logo.png";
+import burger from "../../icons/burger.png";
+import closed from "../../icons/closed.png";
 import './header.css';
+import { useState } from 'react';
+
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <header>
       <nav>
-        <img src={logo} alt="circle logo with building instruments"/>
-        <Link to="/" className="link">Home</Link>
-        <Link to="/about" className="link">About Us</Link>
-        <Link to="/service" className="link">Our services</Link>
-        <Link to="/maintenance" className="link">Maintenance Service</Link>
-        <Link to="/contact" className="link">Contact Us</Link>
+        <img src={logo} className="logo" alt="circle logo with building instruments"/>
+
+        <div className='menu' onClick={() => setMenuOpen(!menuOpen)}>
+          <img 
+          src={menuOpen ? closed : burger} 
+          alt={menuOpen ? "mobile menu closed button" : "mobile menu open button"} 
+          />
+        </div>
+
+        <div id="links" className={menuOpen ? "open" : ""}>
+          <Link to="/" className="link">Home</Link>
+          <Link to="/about" className="link">About</Link>
+          <Link to="/service" className="link">Services</Link>
+          <Link to="/maintenance" className="link">Property maintenance</Link>
+          <Link to="/contact" className="link">Contact</Link>
+        </div>
       </nav>
-    </header>
+    
   )
 }
 
